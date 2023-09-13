29e7ca877b110f9d340ec3ff1e7e75cf364fbd9a
YEEUNco <121320706+YEEUNco@users.noreply.github.com>
2023-09-14 00:40:01 +0900

    Update nmn.cpp

--- Lab04/nmn.cpp
+++ Lab04/nmn.cpp
@@ -49,7 +49,7 @@ int main() {
 	int arr[11] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10};
 	
 	//arr[10] = 10;
-	printList(arr, sizeof(arr)/sizeof(int));
+	printList(arr, sizeof(arr)/sizeof(arr[0));
 
 	return 0;
 }
